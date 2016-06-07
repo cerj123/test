6fa2e5d22df851d423f492f432fa02bb1da92da1
Vincent <cerjart@gmail.com>
2016-06-07 19:53:43 +0300

    Delete semver.js

--- semver.js
+++
@@ -1,8 +0,0 @@
-var semver = require('semver');
-
-console.log(semver.valid('1.2.3')); // '1.2.3'
-console.log(semver.valid('a.b.c')); // null
-console.log(semver.clean('  =v1.2.3   ')); // '1.2.3'
-console.log(semver.satisfies('1.2.3', '1.x || >=2.5.0 || 5.0.0 - 7.2.3')); // true
-console.log(semver.gt('1.2.3', '9.8.7')); // false
-console.log(semver.lt('1.2.3', '9.8.7')); // true
